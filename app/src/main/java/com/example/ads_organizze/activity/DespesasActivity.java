9c64c5170326a6ebeea86c1c232b178b0d1cde1d
Adson-C <adsonconcecao@yahoo.com.br>
2023-07-25 12:06:38 -0300

    Salvando Desepesas

--- app/src/main/java/com/example/ads_organizze/activity/DespesasActivity.java
+++ app/src/main/java/com/example/ads_organizze/activity/DespesasActivity.java
@@ -3,16 +3,19 @@ package com.example.ads_organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import com.example.ads_organizze.R;
 import com.example.ads_organizze.helper.DateCustom;
+import com.example.ads_organizze.model.Movimentacao;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DespesasActivity extends AppCompatActivity {
 
     private TextInputEditText campoData, campoCategoria, campoDescricao;
     private EditText campoValor;
+    private Movimentacao movimentacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +29,18 @@ public class DespesasActivity extends AppCompatActivity {
 
         // Preecher o campo data com a date atual
         campoData.setText(DateCustom.dataAtual());
+    }
+
+    // salvar despesas
+    public void salvarDespesa(View view){
+        movimentacao = new Movimentacao();
+        String data = campoData.getText().toString();
+        movimentacao.setValor(String.valueOf(Double.parseDouble(campoValor.getText().toString())));
+        movimentacao.setCategoria(campoCategoria.getText().toString());
+        movimentacao.setDescricao(campoDescricao.getText().toString());
+        movimentacao.setData( data);
+        movimentacao.setTipo("d");
+
+        movimentacao.salvar( data );
     }
 }
--- app/src/main/java/com/example/ads_organizze/helper/DateCustom.java
+++ app/src/main/java/com/example/ads_organizze/helper/DateCustom.java
@@ -6,11 +6,23 @@ public class DateCustom {
 
     public static String dataAtual(){
 
+    // converter data e dataAtual
     long data = System.currentTimeMillis();
     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     String dataString = sdf.format(data);
 
     return dataString;
-
     }
+
+    // retorno data mes e ano
+    public static String mesAnoDataEscolihdo(String data){
+        // regex 23/08/2023
+           String retornoData[] = data.split("/");
+           String dia = retornoData[0]; // dia 23
+           String mes = retornoData[1]; // mes 08
+           String ano = retornoData[2]; // ano 2023
+            String mesAno = mes + ano;
+            return mesAno;
+    }
+
 }
